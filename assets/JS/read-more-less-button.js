e826e3ca1f739bf1b814bde7cde18fb36fc5c168
alvesjaov <victoralves169@outlook.com>
2023-09-23 19:37:37 -0300

    fiz a página de certificados (ainda falta fazer a descrição de 5)

---
+++ assets/JS/read-more-less-button.js
@@ -0,0 +1,19 @@
+function myFunction(moreTextId, dotsId, btnId) {
+  // Obter os elementos do DOM usando os IDs fornecidos
+  var moreText = document.getElementById(moreTextId); // O texto que será mostrado ou escondido
+  var dots = document.getElementById(dotsId); // Os pontos que serão mostrados ou escondidos
+  var btn = document.getElementById(btnId); // O botão que o usuário clica para mostrar ou esconder o texto
+
+  // Se o texto estiver escondido (display = "none"), mostre-o
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline"; // Mostra o texto
+    btn.innerHTML = "Ler menos"; // Muda o texto do botão para "Ler menos"
+  } else {
+// Se o texto estiver sendo mostrado, esconda-o
+    moreText.style.display = "none"; // Esconde o texto
+    btn.innerHTML = "Ler mais"; // Muda o texto do botão para "Ler mais"
+  }
+  
+  // Alterna a exibição dos pontos com base na exibição do texto
+  dots.style.display = (moreText.style.display === "none") ? "inline" : "none";
+}
